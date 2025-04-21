f3a2070f2cf4958fecf232282020e24d611d493d
juanDev-UV <199684711+juanDev-UV@users.noreply.github.com>
2025-04-20 19:42:47 -0500

    This update includes the 3D model of the respiratory system with directional lighting, floor and shadows, along with the written description of Bronchitis

--- src/pages/lung/bronchitis/Bronchitis.jsx
+++ src/pages/lung/bronchitis/Bronchitis.jsx
@@ -6,6 +6,8 @@ import Nurse from "./models-3d/Nurse";
 import LungRith from "./models-3d/LungRight";
 import HealthPack from "./models-3d/HealthPack";
 import LungTransparent from "./models-3d/LungTransparent";
+import LightsLungRight from "./lights/LightsLungRight";
+import Floor from "./models-3d/Floor";
 
 const Bronchitis = () => {
   return (
@@ -17,22 +19,26 @@ const Bronchitis = () => {
           <div className="text-content">
             <h2 className="title title-intro">Bronquitis</h2>
             <p className="text text-intro">
-              La bronquitis es la inflamación de los bronquios, los vías 
-              respiratorias que llevan el aire a los pulmones. Puede ser 
-              aguda, causada generalmente por infecciones o crónica, 
-              vinculada al tabaquismo o la exposición prolongada a 
-              contaminantes. Su principal consecuencia es la dificultad 
-              para respirar debido a la producción excesiva de moco y 
+              La bronquitis es la inflamación de los bronquios, los vías
+              respiratorias que llevan el aire a los pulmones. Puede ser
+              aguda, causada generalmente por infecciones o crónica,
+              vinculada al tabaquismo o la exposición prolongada a
+              contaminantes. Su principal consecuencia es la dificultad
+              para respirar debido a la producción excesiva de moco y
               la inflamación de los conductos bronquiales.
             </p>
           </div>
           <div className="model model-lung-right" >
-                  <Canvas camera={{position: [0, 0,3]}}>
-                  <OrbitControls />
-                  <ambientLight intensity={1.5} />
-                  <directionalLight position={[5, 2, 10]} intensity={2} />
-                  <LungRith scale={11} />
-                  </Canvas>
+            <Canvas camera={{ position: [0, 0, 3] }} shadows={true}>
+              <OrbitControls />
+              <LightsLungRight />
+              <directionalLight
+                position={[5, 2, 10]}
+                intensity={2}
+                castShadow={true} />
+              <LungRith scale={11} />
+              <Floor/>
+            </Canvas>
           </div>
         </div>
         <div className="arrow arrow-intro">▼</div>
@@ -44,20 +50,24 @@ const Bronchitis = () => {
           <div className="text-content">
             <h3 className="title title-symptoms">Síntomas</h3>
             <p className="text text-symptoms">
-              Los síntomas más comunes incluyen tos persistente con 
-              flema, fatiga, fiebre leve y dificultad para respirar. Su 
-              diagnóstico se realiza mediante examen físico, 
-              radiografías de tórax y análisis de esputo para descartar 
+              Los síntomas más comunes incluyen tos persistente con
+              flema, fatiga, fiebre leve y dificultad para respirar. Su
+              diagnóstico se realiza mediante examen físico,
+              radiografías de tórax y análisis de esputo para descartar
               infecciones bacterianas.
             </p>
           </div>
           <div className="model model-lung-transparent" >
-                  <Canvas camera={{position: [-0.5, 0,2]}}>
-                  <OrbitControls />
-                  <ambientLight intensity={1.5} />
-                  <directionalLight position={[5, 2, 10]} intensity={2} />
-                  <LungTransparent scale={5} />
-                  </Canvas>
+            <Canvas camera={{ position: [-0.5, 0, 2] }}>
+              <OrbitControls />
+              <ambientLight
+                intensity={1.5} />
+              <directionalLight
+                position={[5, 2, 10]}
+                intensity={2} />
+              <LungTransparent
+                scale={5} />
+            </Canvas>
           </div>
         </div>
         <div className="arrow arrow-symptoms">▼</div>
@@ -69,21 +79,21 @@ const Bronchitis = () => {
           <div className="text-content">
             <h3 className="title title-treatment">Tratamiento</h3>
             <p className="text text-treatment">
-              El tratamiento depende del tipo de bronquitis. En la 
-              mayoría de los casos, se recomiendan hidratación, 
-              descanso, y en casos graves, broncodilatadores o 
-              corticosteroides. Para prevenir, se debe evitar el humo del 
-              tabaco, mantenerse alejado de contaminantes y 
+              El tratamiento depende del tipo de bronquitis. En la
+              mayoría de los casos, se recomiendan hidratación,
+              descanso, y en casos graves, broncodilatadores o
+              corticosteroides. Para prevenir, se debe evitar el humo del
+              tabaco, mantenerse alejado de contaminantes y
               vacunarse contra la gripe.
             </p>
           </div>
           <div className="model model-nurse" >
-                  <Canvas camera={{position: [0, 0,3]}}>
-                  <OrbitControls />
-                  <ambientLight intensity={1.5} />
-                  <directionalLight position={[5, 2, 10]} intensity={2} />
-                  <Nurse scale={5} />
-                  </Canvas>
+            <Canvas camera={{ position: [0, 0, 3] }}>
+              <OrbitControls />
+              <ambientLight intensity={1.5} />
+              <directionalLight position={[5, 2, 10]} intensity={2} />
+              <Nurse scale={5} />
+            </Canvas>
           </div>
         </div>
         <div className="arrow arrow-treatment">▼</div>
@@ -95,19 +105,19 @@ const Bronchitis = () => {
           <div className="text-content">
             <h3 className="title title-cases">Casos</h3>
             <p className="text text-cases">
-              En casos crónicos, la bronquitis puede derivar en 
-              enfermedad pulmonar obstructiva crónica (EPOC), en cuyo 
-              caso el paciente sufre de tos persistente durante años. Es 
+              En casos crónicos, la bronquitis puede derivar en
+              enfermedad pulmonar obstructiva crónica (EPOC), en cuyo
+              caso el paciente sufre de tos persistente durante años. Es
               un mito que los antibióticos siempre curan la bronquitis, ya que solo son efectivos en infecciones bacterianas, no en las virales.
             </p>
           </div>
           <div className="model model-health-pack" >
-                  <Canvas camera={{position: [3, 0, 5]}}>
-                  <OrbitControls />
-                  <ambientLight intensity={1.5} />
-                  <directionalLight position={[5, 2, 10]} intensity={2} />
-                  <HealthPack scale={4} />
-                  </Canvas>
+            <Canvas camera={{ position: [3, 0, 5] }}>
+              <OrbitControls />
+              <ambientLight intensity={1.5} />
+              <directionalLight position={[5, 2, 10]} intensity={2} />
+              <HealthPack scale={4} />
+            </Canvas>
           </div>
         </div>
       </section>
--- src/pages/lung/bronchitis/models-3d/LungRight.jsx
+++ src/pages/lung/bronchitis/models-3d/LungRight.jsx
@@ -1,73 +1,75 @@
 /* eslint-disable react/no-unknown-property */
 import { useGLTF } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
 
 const LungRith = (props) => {
+
+  const lungRightRef = useRef();
+
+  useFrame((state, delta) => {
+    lungRightRef.current.rotation.y +=1 * delta;
+  })
   const { nodes, materials } = useGLTF("/models-3d/lung-right.glb");
   return (
     <group {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.RespiratoryLigament.geometry}
-        material={materials.LungRightMaterial}
-        position={[-0.001, -0.1, -0.077]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.RespiratoyEpiglotis_1.geometry}
-        material={materials.LungRightMaterial}
-        position={[-0.001, -0.1, -0.077]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.RespiratoyEpiglotisInner_1.geometry}
-        material={materials.LungRightMaterial}
-        position={[-0.001, -0.1, -0.077]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.RespiratoyThyroidCartliage_1.geometry}
-        material={materials.LungRightMaterial}
-        position={[-0.001, -0.1, -0.077]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.RespiratoyThyroidMembrane_1.geometry}
-        material={materials.LungRightMaterial}
-        position={[-0.001, -0.1, -0.077]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.RespiratoyVocalFoldLeft_1.geometry}
-        material={materials.LungRightMaterial}
-        position={[-0.001, -0.1, -0.077]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.RespiratoyVocalFoldRight_1.geometry}
-        material={materials.LungRightMaterial}
-        position={[-0.001, -0.1, -0.077]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.RespiratoyLungGeoRightMale.geometry}
-        material={materials.RespiratoryLungMaterial}
-        position={[-0.001, -0.1, -0.077]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.RespiratoyTracheaGeoMale.geometry}
-        material={materials.TraqueaMaterial}
-        position={[-0.001, -0.1, -0.077]}
-      />
+      <group ref={lungRightRef} position={[-0.001, -0.1, -0.077]} >
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.RespiratoryLigament.geometry}
+          material={materials.LungRightMaterial}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.RespiratoyEpiglotis_1.geometry}
+          material={materials.LungRightMaterial}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.RespiratoyEpiglotisInner_1.geometry}
+          material={materials.LungRightMaterial}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.RespiratoyThyroidCartliage_1.geometry}
+          material={materials.LungRightMaterial}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.RespiratoyThyroidMembrane_1.geometry}
+          material={materials.LungRightMaterial}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.RespiratoyVocalFoldLeft_1.geometry}
+          material={materials.LungRightMaterial}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.RespiratoyVocalFoldRight_1.geometry}
+          material={materials.LungRightMaterial}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.RespiratoyLungGeoRightMale.geometry}
+          material={materials.RespiratoryLungMaterial}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.RespiratoyTracheaGeoMale.geometry}
+          material={materials.TraqueaMaterial}
+        />
+
+      </group>
     </group>
   )
 }
