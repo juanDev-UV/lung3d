f7e872055429fd4efc99bd491b18aa7c50fb34b2
Leal2005 <alan.leal@correounivalle.edu.co>
2025-05-11 01:54:18 -0500

    Implementation of 3D models, lighting, shadows, interactivity, and HTML in 3D.

--- src/pages/lung/pneumonia/lights/Lights.jsx
+++ src/pages/lung/pneumonia/lights/Lights.jsx
@@ -2,29 +2,49 @@ import { useHelper } from "@react-three/drei";
 import { useRef } from "react";
 
 /* eslint-disable react/no-unknown-property */
-const Lights = () => {
-    const directionalLightRef = useRef();
-    useHelper(directionalLightRef);
+const Lights = ({ type }) => {
+  const directionalLightRef = useRef();
+  useHelper(directionalLightRef);
 
-    return (
-      <>
-        <ambientLight intensity={0.5} /> {/* Luz general suave */}
-        <directionalLight
-          ref={directionalLightRef}
-          position={[0, 5, 5]}
-          intensity={1.2}
-          castShadow
-          shadow-mapSize-width={2048}
-          shadow-mapSize-height={2048}
-          shadow-camera-left={-10}
-          shadow-camera-right={10}
-          shadow-camera-top={10}
-          shadow-camera-bottom={-10}
-          shadow-camera-near={0.5}
-          shadow-camera-far={50}
-        />
-      </>
-    );
-}
+  return (
+    <>
+      {type === "A" && (
+        <>
+          <ambientLight intensity={0.5} />
+          <directionalLight
+            ref={directionalLightRef}
+            position={[0, 5, 5]}
+            intensity={1.3}
+            castShadow
+            shadow-mapSize-width={4096}
+            shadow-mapSize-height={4096}
+            shadow-camera-left={-10}
+            shadow-camera-right={10}
+            shadow-camera-top={10}
+            shadow-camera-bottom={-10}
+            shadow-camera-near={0.5}
+            shadow-camera-far={50}
+          />
+        </>
+      )}
+
+      {type === "B" && (
+        <>
+          <ambientLight intensity={0.9} />
+          <spotLight
+            ref={directionalLightRef}
+            position={[5, 10, 5]}
+            intensity={2}
+            angle={0.6}
+            penumbra={0.3}
+            castShadow
+            shadow-mapSize-width={2048}
+            shadow-mapSize-height={2048}
+          />
+        </>
+      )}
+    </>
+  );
+};
 
 export default Lights;
